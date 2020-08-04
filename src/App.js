ab9c043ebc0b1f7cab1b3ccd95f2b52f4fccddf9
CheaC <cheacliatt@gmail.com>
2020-08-04 18:37:48 -0400

    Got my Home Page and began doing the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
+import Home from "./Pages/Home"
 
 function App() {
   return (
-    <Nav />
+    <Router>
+      <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,11 +3,10 @@ import "./style.css";
 
 function Nav() {
   return (
-
-
-
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="index.html">Chea Cliatt</a>
+      <a className="navbar-brand" href="index.html">
+        Chea Cliatt
+      </a>
       <button
         className="navbar-toggler custom-toggler"
         type="button"
@@ -23,26 +22,24 @@ function Nav() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active ml-auto">
-            <a className="nav-link" href="index.html">Home</a>
+            <a className="nav-link" href="index.html">
+              Home
+            </a>
           </li>
           <li className="nav-item ml-auto">
-            <a className="nav-link" href="contact.html">Contact Me</a>
+            <a className="nav-link" href="contact.html">
+              Contact Me
+            </a>
           </li>
           <li className="nav-item ml-auto">
-            <a className="nav-link" href="portfolio.html"
-              >Portfolio <span className="sr-only">(current)</span>
+            <a className="nav-link" href="portfolio.html">
+              Portfolio <span className="sr-only">(current)</span>
             </a>
           </li>
         </ul>
       </div>
     </nav>
-
-
-
-  )
-  
-  
-
+  );
 }
 
 export default Nav;
