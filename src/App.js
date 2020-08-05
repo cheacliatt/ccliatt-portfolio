19f7a0da9771d2a66855ff2eed01b11295554b4f
CheaC <cheacliatt@gmail.com>
2020-08-05 18:16:05 -0400

    Set up contact page. Now just need the portfolio. Router links seem weird

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./Pages/Home";
+import Contact from "./Pages/Contact"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </div>
     </Router>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="index.html">
+      <Link className="navbar-brand" to="/">
         Chea Cliatt
-      </a>
+      </Link>
       <button
         className="navbar-toggler custom-toggler"
         type="button"
@@ -22,19 +23,19 @@ function Nav() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active ml-auto">
-            <a className="nav-link" href="/home">
+            <Link className="nav-link" to="/home">
               Home
-            </a>
+            </Link>
           </li>
           <li className="nav-item ml-auto">
-            <a className="nav-link" href="contact.html">
+            <Link className="nav-link" to="/contact">
               Contact Me
-            </a>
+            </Link>
           </li>
           <li className="nav-item ml-auto">
-            <a className="nav-link" href="portfolio.html">
+            <Link className="nav-link" to="/portfolio">
               Portfolio <span className="sr-only">(current)</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
